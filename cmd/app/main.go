59559a28a9aa9bab48ca77f5d6b95f54d87e82fe
Dmitry Rakovets <dmitryrakovets@gmail.com>
2023-02-18 07:49:37 +0300

    feat: init App

---
+++ cmd/app/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/rakovets/doc-tools/internal/app"
+
+func main() {
+	app.Run()
+}
---
+++ internal/app/app.go
@@ -0,0 +1,10 @@
+package app
+
+import (
+	"log"
+)
+
+func Run() {
+	log.Printf("INFO : Application started")
+	log.Printf("INFO : Application finished")
+}
